3b80b4882db87bff833f7f535f0151e29c69ee86
Philip Horger <campadrenalin@gmail.com>
2012-02-26 14:33:47 -0800

    Fixed expansion incompatibility in install-wildly.sh

--- install-wildly.sh
+++ install-wildly.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget http://orchard.crabdance.com/desktop/c/libevent.deb
 wget https://github.com/campadrenalin/cjdns-deb/raw/master/cjdns.deb
-sudo dpkg -i {libevent,cjdns}.deb
+sudo dpkg -i libevent.deb cjdns.deb
 sudo cjdrouted start-default
