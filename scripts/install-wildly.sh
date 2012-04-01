50e0ed41c2734a617faa6baa8faf02525df996ee
Philip Horger <campadrenalin@gmail.com>
2012-04-01 10:17:57 -0700

    Deleted some pesky swp files and updated install-wildly with correct links.

--- scripts/install-wildly.sh
+++ scripts/install-wildly.sh
@@ -9,7 +9,7 @@ fi
 
 # TODO: multiple builds of libevent
 wget -O libevent.deb http://orchard.crabdance.com/desktop/c/libevent.deb
-wget -O cjdns.deb --no-check-certificate https://github.com/campadrenalin/cjdns-deb/raw/master/latest/cjdns-$ARCH.deb
-wget -O cjconf.deb --no-check-certificate https://github.com/campadrenalin/cjconf/raw/master/latest/cjconf.deb
+wget -O cjdns.deb --no-check-certificate https://github.com/campadrenalin/cjdns-deb/raw/master/output/latest/cjdns-$ARCH.deb
+wget -O cjconf.deb --no-check-certificate https://github.com/campadrenalin/cjconf/raw/master/output/latest/cjconf.deb
 sudo dpkg -i libevent.deb cjdns.deb cjconf.deb
 sudo cjdrouted start-default
