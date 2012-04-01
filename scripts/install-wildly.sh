b115060ab6f2aa6c28e7fca5f8038a37acc935cf
Philip Horger <campadrenalin@gmail.com>
2012-03-31 21:33:21 -0700

    Semi-automatic build: 0.3-200

--- scripts/install-wildly.sh
+++ scripts/install-wildly.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wget -O libevent.deb http://orchard.crabdance.com/desktop/c/libevent.deb
-wget -O cjdns.deb --no-check-certificate https://github.com/campadrenalin/cjdns-deb/raw/master/cjdns.deb
-sudo dpkg -i libevent.deb cjdns.deb
+wget -O cjdns.deb --no-check-certificate https://github.com/campadrenalin/cjdns-deb/raw/master/latest.deb
+wget -O cjconf.deb --no-check-certificate https://github.com/campadrenalin/cjconf/raw/master/latest.deb
+sudo dpkg -i libevent.deb cjdns.deb cjconf.deb
 sudo cjdrouted start-default
