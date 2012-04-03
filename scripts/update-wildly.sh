e31b8f19804fd34dc0449f0cb85998187fec5deb
Philip Horger <campadrenalin@gmail.com>
2012-04-03 02:16:47 -0700

    Semi-automatic build: 0.3-202

--- scripts/update-wildly.sh
+++ scripts/update-wildly.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-wget -O cjdns.deb --no-check-certificate https://github.com/campadrenalin/cjdns-deb/raw/master/cjdns.deb
-sudo dpkg -i cjdns.deb
+wget -O cjdns.deb --no-check-certificate https://github.com/campadrenalin/cjdns-deb/raw/master/output/latest/cjdns-i386.deb
+wget -O cjconf.deb --no-check-certificate https://github.com/campadrenalin/cjconf/raw/master/output/latest/cjconf.deb
+sudo dpkg -i cjdns.deb cjconf.deb
